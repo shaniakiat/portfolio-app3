9d62369cf66d276aa0a6dfc747ee44f84b38c3e5
shaniakiat <kiat.shania@gmail.com>
2020-06-15 21:28:47 -0400

    remodifying SEO 3

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,6 +71,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `twitter:image`,
+          content: site.siteMetadata.image,
+        },
       ].concat(meta)}
     />
   )
